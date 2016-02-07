4908869748efdcb54b23da414c5f165d31f45a5f
Jonathan Hoyt <hoyt@github.com>
2016-02-06 23:01:12 -0800

    Implement DataSet with tests

--- app/DataSet.js
+++ app/DataSet.js
@@ -1,7 +1,62 @@
-'use strict';
+import fs from "fs"
+import parse from "csv-parse"
 
 export default class {
-  load() {
-    return "loaded"
+  constructor(path) {
+    this.loaded = false
+    this.data = []
+    this.seconds = 0.0
+
+    this.parser = parse({auto_parse: true})
+    this.parser.on("readable", ()=>{
+      var record;
+      while(record = this.parser.read()) {
+        this.data.push(record)
+        this.seconds = record[0]
+      }
+    })
+    this.parser.on("error", (error)=>{
+      console.log(error.message)
+    })
+
+    this.stream = fs.createReadStream(path)
+    this.stream.on("end", ()=>{
+      if(this.data[0][0] == "Time[s]") {
+        this.data.shift()
+      }
+      this.loaded = true
+    })
+    this.stream.pipe(this.parser)
+  }
+
+  dataForTimespan(start_time, end_time) {
+    function binaryIndexOf(searchElement) {
+      var minIndex = 0
+      var maxIndex = this.length - 1
+      var currentIndex
+      var currentElement
+
+      while (minIndex <= maxIndex) {
+        currentIndex = (minIndex + maxIndex) / 2 | 0;
+        currentElement = this[currentIndex]
+
+        if (currentElement[0] < searchElement) {
+          minIndex = currentIndex + 1
+        }
+        else if (currentElement[0] > searchElement) {
+          maxIndex = currentIndex - 1
+        }
+        else {
+          return currentIndex
+        }
+      }
+
+      return currentIndex + 1
+    }
+
+    var start_index = binaryIndexOf.call(this.data, start_time)
+    var end_index = binaryIndexOf.call(this.data, end_time)
+
+    return this.data.slice(start_index, end_index)
   }
 }
--- spec/DataSetSpec.js
+++ spec/DataSetSpec.js
@@ -1,12 +1,41 @@
-"use strict";
-
 import DataSet from "../app/DataSet"
+const path = __dirname + "/fixtures/hello_world.csv"
 
 describe("DataSet", ()=>{
-  describe("load", ()=>{
-    it("is loaded", ()=>{
-      var dataset = new DataSet
-      expect(dataset.load()).toEqual("loaded")
+  var dataset = new DataSet(path)
+
+  beforeAll((done)=>{
+    setTimeout(()=>{
+      done()
+    }, 100)
+  })
+
+  describe("loaded", ()=>{
+    it("returns true when data has loaded", ()=>{
+      expect(dataset.loaded).toBe(true)
+    })
+  })
+
+  describe("data", ()=>{
+    it("returns loaded data", ()=>{
+      expect(dataset.data.length).toEqual(999)
+    })
+  })
+
+  describe("seconds", ()=>{
+    it("returns seconds in set", ()=>{
+      expect(dataset.seconds).toEqual(0.172783833333333)
+    })
+  })
+
+  describe("dataForTimespan", ()=>{
+    it("returns data for timespan", ()=>{
+      expect(dataset.dataForTimespan(0.0014, 0.0019)).toEqual([
+        [0.001439333333333,1],
+        [0.001543333333333,0],
+        [0.001751416666667,1],
+        [0.001855500000000,0]
+      ])
     })
   })
 })
